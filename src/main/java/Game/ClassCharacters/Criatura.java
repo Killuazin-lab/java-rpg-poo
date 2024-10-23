dc9c2dea494b7f862f45b285cfe1aac5a924264f
Gabriel Frigo <gfrigo.sena@gmail.com>
2024-10-23 19:24:55 -0300

    create-super-class-creature

---
+++ src/main/java/Game/ClassCharacters/Criatura.java
@@ -0,0 +1,47 @@
+package Game.ClassCharacters;
+
+public class Criatura {
+  protected String nome;
+  protected int vida;
+  protected int ataque;
+  protected int defesa;
+
+  public Criatura(String nome, int vida, int ataque, int defesa){
+    this.nome = nome;
+    this.vida = vida;
+    this.ataque = ataque;
+    this.defesa = defesa;
+  }
+
+  public int getAtaque() {
+    return ataque;
+  }
+
+  public void setAtaque(int ataque) {
+    this.ataque = ataque;
+  }
+
+  public int getDefesa() {
+    return defesa;
+  }
+
+  public void setDefesa(int defesa) {
+    this.defesa = defesa;
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public int getVida() {
+    return vida;
+  }
+
+  public void setVida(int vida) {
+    this.vida = vida;
+  }
+}
